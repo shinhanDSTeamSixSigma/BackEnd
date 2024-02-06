cd1d8d179bf4e54a88b99407f8e82f63106dd5ee
EricSamuelJun <mam675@naver.com>
2024-02-06 12:59:51 +0900

    initial Commit

---
+++ src/main/java/site/greenwave/GreenwaveApplication.java
@@ -0,0 +1,13 @@
+package site.greenwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenwaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenwaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/greenwave/GreenwaveApplicationTests.java
@@ -0,0 +1,13 @@
+package site.greenwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenwaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
