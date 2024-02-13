49cfe6a6cacb590a1424b2384df2f851a8f72ee2
EricSamuelJun <mam675@naver.com>
2024-02-13 19:48:05 +0900

    feat. 파일 업로드 / 다운로드 작업 완료

--- src/main/java/site/greenwave/config/WebMvcConfig.java
+++ src/main/java/site/greenwave/config/WebMvcConfig.java
@@ -1,16 +1,19 @@
 package site.greenwave.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+	@Value("${filesave.location}")
+	private String path;
+	
 	@Override
 	public void  addResourceHandlers(ResourceHandlerRegistry registry) {
+		String resourceLocationPath = "file:///"+path;
 		registry.addResourceHandler("img/**")
-				.addResourceLocations("file:///C:/greenwave/img/")//TestLocal Window
-//				.addResourceLocations("file:///C:/greenwave/img/")//TestLocal MacBook
-//				.addResourceLocations("file:///home/img/")//Deploy
+				.addResourceLocations(resourceLocationPath)
 				.setCachePeriod(31556926);
 	}
 }
