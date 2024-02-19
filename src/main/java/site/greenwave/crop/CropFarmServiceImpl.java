1a44d6046c834cea67c7f9b0f14ea9966480518c
EricSamuelJun <mam675@naver.com>
2024-02-19 11:24:50 +0900

    Feat. 농장 별 Crop 출력

---
+++ src/main/java/site/greenwave/crop/CropFarmServiceImpl.java
@@ -0,0 +1,21 @@
+package site.greenwave.crop;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
+public class CropFarmServiceImpl implements CropFarmService {
+	@Autowired
+	CropMapper cropMapper;
+	@Override
+	public List<Map> getCropsFromFarmNoAndMemberNo(int farmNo, int memberNo) {
+		Map<String, Integer> map = new HashMap<>();
+		map.put("member_no", memberNo);
+		map.put("farm_no", farmNo);
+		return cropMapper.getCropsFromMemberNoAndFarmNo(map);
+	}
+
+}
