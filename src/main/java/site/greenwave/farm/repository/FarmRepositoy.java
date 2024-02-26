ddbea919d83ad6c7ed3db1154590cae356a8850f
dnscodnjs <yun9869@naver.com>
2024-02-26 14:40:14 +0900

    Feat : 검색 기능 추가

--- src/main/java/site/greenwave/farm/repository/FarmRepositoy.java
+++ src/main/java/site/greenwave/farm/repository/FarmRepositoy.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import site.greenwave.farm.entity.FarmEntity;
 
+import java.util.List;
+
 @Repository
 public interface FarmRepositoy extends JpaRepository<FarmEntity,Integer> {
+    // 농장 이름으로 부분 일치 검색
+    List<FarmEntity> findByFarmNameContainingIgnoreCase(String name);
 
 }
