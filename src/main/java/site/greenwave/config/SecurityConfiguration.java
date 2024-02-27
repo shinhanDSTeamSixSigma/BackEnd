3a945b108334e6b5516c43e6bab032185790bd91
EricSamuelJun <mam675@naver.com>
2024-02-27 14:53:18 +0900

    Feat. 배포 직전 머지 작업 및 수정

--- src/main/java/site/greenwave/config/SecurityConfiguration.java
+++ src/main/java/site/greenwave/config/SecurityConfiguration.java
@@ -52,6 +52,7 @@ public class SecurityConfiguration {
             	List<String> originList = new ArrayList<>();
             	originList.add("http://localhost:3000");
             	originList.add("http://localhost:5500");
+            	originList.add("http://58.122.202.23");
             	originList.add("http://192.168.0.200"); // 아두이노
                 CorsConfiguration config = new CorsConfiguration();
                 config.setAllowedOrigins(originList);
